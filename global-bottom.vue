5acc518f952decc169c6baf04920c9e9ed328959
Clydinite <clydinite@gmail.com>
2026-01-02 02:54:54 +0800

    added page number

---
+++ global-bottom.vue
@@ -0,0 +1,9 @@
+<!-- an example footer for pages -->
+<template>
+  <footer
+    v-if="$nav.currentLayout !== 'cover'"
+    class="absolute bottom-0 right-1 p-2 text-sm text-gray-500"
+  >
+    {{ $nav.currentPage }}
+  </footer>
+</template>
